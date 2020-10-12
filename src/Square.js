db5381a7ff8d9c0ddd822dbd744e6d5abee369eb
caemci <carlosecimino@gmail.com>
2020-10-12 19:27:32 -0300

    Squares con estado

--- src/Square.js
+++ src/Square.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 class Square extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: null
+		};
+	}
+
+	handleClick = (e) => {
+		this.setState({ value: 'X' });
+	}
+
 	render() {
 		return (
-			<button className="square">
-				{this.props.value}
+			<button className="square" onClick={this.handleClick}>
+				{this.state.value}
 			</button>
 		);
 	}
